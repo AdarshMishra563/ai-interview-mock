efdbae752e28f8bddceeea4f580c3dca892bb52d
AdarshMishra563 <am0070563@gmail.com>
2025-06-21 17:03:15 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -8,7 +8,17 @@ import { useSelector } from 'react-redux';
 function Page() {
   const navigation=useRouter()
 
- 
+ const k=useSelector((state)=>state?.user?.user);
+
+
+ useEffect(()=>{
+
+  if(k?.token && k?.user){
+    navigation.push("/interview")
+  }else{
+    navigation.push("/login")
+  }
+ },[k?.token])
   return (
     <div  >Interview Loading.....</div>
   )
