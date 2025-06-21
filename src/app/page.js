41f857a24a271223bb0e5d6b130d9ce5bc4d897a
AdarshMishra563 <am0070563@gmail.com>
2025-06-21 16:22:42 +0530

    new

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 'use client'
+import { useRouter } from 'next/navigation';
 import React, { useEffect } from 'react'
 
-import { useRouter } from 'next/navigation'
+
 import { useSelector } from 'react-redux';
 
 function Page() {
-  const navigation=useRouter();
+  const navigation=useRouter()
 
  const {token,user}=useSelector((state)=>state.user.user);
 
