9f3fb571faf46d18c86f69887df86e7a9cc14133
Adarsh Mishra <am0070563@gmail.com>
2025-06-21 17:47:23 +0530

    Update page.js

--- src/app/interview/page.js
+++ src/app/interview/page.js
@@ -110,7 +110,7 @@ const dispatch=useDispatch();
     recognition.onerror = () => stopListening();
     recognition.onend = () => {
       console.log(' Voice recognition ended');
-      setTimeout(()=>{ startListening()},1500)
+      setTimeout(()=>{ startListening()},1000)
      
    
     };
